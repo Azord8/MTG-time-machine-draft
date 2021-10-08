251d10ae01a619cf4c56396cc3d6ecbf142947a7
joshmoorer@gmail.com <77P$XX*d34Aq>
2021-10-07 19:57:33 -0500

    Adding jQuery

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def find_sets(date):
 def create_booster(setcode):
     db = Mongo.get_db(MongoDBconnectString, local)
     boosters = db.Boosters.find_one({'_id': setcode})
+    print(boosters)
     weights = []
     booster = []
     for i in range(len(boosters['Boosters'])):
